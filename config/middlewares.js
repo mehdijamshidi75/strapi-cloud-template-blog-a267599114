686790a6181b8959887c66032ec7ec452bbe4e17
mehdi <mehdi.jamshidi75@gmail.com>
2025-11-14 20:46:28 +0330

    feat: Update CORS policy to allow all origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,19 @@
+// file: config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // <-- این به همه دامنه‌ها اجازه می‌دهد (برای تست)
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
