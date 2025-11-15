c1c7c6a6cf3b628988b1ded16c174635a8ce4e36
mehdi <mehdi.jamshidi75@gmail.com>
2025-11-15 21:07:27 +0330

    fix: final correct CORS configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,15 +3,10 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   {
-    // ما رشته 'strapi::cors' را با این آبجکت جایگزین می‌کنیم
     name: 'strapi::cors',
     config: {
       enabled: true,
-      // به همه‌جا اجازه می‌دهد (برای تست)
-      origin: ['*'], 
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
+      origin: ['*'], // به همه دامنه‌ها اجازه بده
     },
   },
   'strapi::poweredBy',
