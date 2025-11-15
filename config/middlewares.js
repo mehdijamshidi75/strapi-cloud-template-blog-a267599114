e6220c1b6dd99622215e38c09cfc14c87e7c8bda
mehdi <mehdi.jamshidi75@gmail.com>
2025-11-15 20:30:51 +0330

    feat: Update CORS policy to allow all origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,25 @@
+// file: config/middlewares.js
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    // ما رشته 'strapi::cors' را با این آبجکت جایگزین می‌کنیم
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      // ما از تنظیماتی که خودتان نوشته بودید + یک ستاره (*) برای اطمینان استفاده می‌کنیم
+      origin: [
+        'https://stackblitz.com',
+        'https://*.stackblitz.io', // برای همه زیردامنه‌های stackblitz
+        'http://localhost:3000',
+        'http://localhost:1337',
+        '*' // به عنوان پشتیبان، به همه‌جا اجازه می‌دهد
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -10,19 +28,3 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
-
-module.exports.settings = {
-  cors: {
-    enabled: true,
-    origin: [
-      'https://*.stackblitz.com',
-      'https://*.stackblitz.io', 
-      'https://*.webcontainer.io',
-      'http://localhost:3000',
-      'http://localhost:1337'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-    keepHeadersOnError: true,
-  },
-};
